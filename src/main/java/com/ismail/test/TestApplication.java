bec4430cfcc33a76e6c50d0a40af93e08e9e7554
Ismail-Taswar <17ps093@bsmrstu.edu.bd>
2025-06-15 23:26:05 +0600

    Initial commit of Spring Boot project

---
+++ src/main/java/com/ismail/test/TestApplication.java
@@ -0,0 +1,25 @@
+package com.ismail.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestApplication {
+	public static void main(String[] args) {
+		System.out.println("hello Worlds") ;
+		System.out.print("test1 ") ;
+		System.out.print("test2") ;
+		System.out.print("123//") ;
+		String name = "123";
+		System.out.println(name) ;
+		int tst = 12 ;
+		System.out.println(tst);
+		float tst2 = 12.12f;
+		System.out.println(tst2) ;
+		float tst23 = 12.12232f ;
+		System.out.println(tst23) ;
+
+		//the next line is needed for web apps
+		SpringApplication.run(TestApplication.class, args);
+	}
+}
