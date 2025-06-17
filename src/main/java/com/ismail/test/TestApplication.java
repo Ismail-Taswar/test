b652e8fd85d64f3a374b0dae1751982d5da7b42b
Ismail-Taswar <17ps093@bsmrstu.edu.bd>
2025-06-18 00:11:11 +0600

    variables

--- src/main/java/com/ismail/test/TestApplication.java
+++ src/main/java/com/ismail/test/TestApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TestApplication {
 	public static void main(String[] args) {
 		System.out.println("hello Worlds") ;
+        /*
 		System.out.print("test1 ") ;
 		System.out.print("test2") ;
 		System.out.print("123//") ;
@@ -45,9 +46,54 @@ public class TestApplication {
 		System.out.println(myiiii);
 		double myDouble = 12.123456789d;
 		int myInt = (int) myDouble; // Manual casting: double to int
-
+        int yx=5;
+        yx+= 55;
+        yx = yx & 3;
+        System.out.println(yx); // Outputs 60
 		System.out.println(myDouble);   // Outputs 9.78
 		System.out.println(myInt);
+        String txt = "ABCDEFGHIJKL\\\MNOPQRSTUVWXYZ";
+        System.out.println(txt.length()) ;
+        System.out.println("aaa".toUpperCase()) ;
+        System.out.println(txt.indexOf("Z"));
+        System.out.println(txt.concat("11"));\
+                System.out.println(Math.max(5,10)+Math.min(1,2)+Math.sqrt(1)+Math.abs(-22)+Math.random()) ;
+        System.out.println(Math.random());
+        if (1>2) {
+            System.out.println(1);
+        } else if (1>2) {
+            System.out.println(2);
+        } else {
+            System.out.println(3);
+        }
+
+        int day = 1;
+        switch (day) {
+            case 6:
+                System.out.println("Today is Saturday");
+                break;
+            case 7:
+                System.out.println("Today is Sunday");
+                break;
+            default:
+                System.out.println("Looking forward to the Weekend");
+        }
+               int a =11;
+        switch(a) {
+          //  default:
+          //      System.out.println(0) ;
+                //break ;
+            case 1:
+                System.out.println(11) ;
+                break ;
+            case 2:
+                System.out.println(121) ;
+                break ;
+            default:
+                System.out.println(0) ;
+		 */
+
+        }
 		//the next line is needed for web apps
 		//SpringApplication.run(TestApplication.class, args);
 	}
