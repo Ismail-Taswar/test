188d9d06b8e7cd3d29936765ba31a9f4008d4edd
Ismail-Taswar <17ps093@bsmrstu.edu.bd>
2025-06-21 19:32:02 +0600

    variables

--- src/main/java/com/ismail/test/Main2.java
+++ src/main/java/com/ismail/test/Main2.java
@@ -4,17 +4,29 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+/*
 public class Main2 {
-    static void myMethod(String fname) {
-        System.out.println(fname + " Refsnes");
+    static int myMethod(int x) {
+        return 6+x ;
     }
-
     public static void main(String[] args) {
-        myMethod("Liam");
-        myMethod("Jenny");
-        myMethod("Anja");
-
-       // SpringApplication.run(Main2.class, args);
+        System.out.println(myMethod(3));
+        System.out.println(sum(10));
     }
 }
+*/
+public class Main2 {
 
+    public static int sum(int start, int end) {
+        if (end > start) {
+            return end + sum(start, end - 1);
+        } else {
+            return end;
+        }
+    }
+    public static void main(String[] args) {
+        int result = sum(5, 10);
+        System.out.println(result);
+    }
+}
+import java.time.LocalTime;
